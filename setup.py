330197d3d181538dea9b48bbe30ec07520896fc6
Marko Ristin <marko@ristin.ch>
2021-04-22 17:58:28 +0200

    Introduce icontract-hypothesis (#2)
    This patch introduces icontract-hypothesis to randomly test a couple of
    functiongs for which we can automatically generte the inputs.

--- setup.py
+++ setup.py
@@ -59,7 +59,8 @@ setuptools.setup(
             "coverage>=4.5.1,<5",
             "docutils>=0.14,<1",
             "icontract>=2.5.0",
-            "prettytable>=2.1.0,<3"
+            "prettytable>=2.1.0,<3",
+            "icontract-hypothesis>=1.1.1,<2"
         ],
     },
     package_data={"thonnycontrib.thonny_sealed": ["py.typed"]},
