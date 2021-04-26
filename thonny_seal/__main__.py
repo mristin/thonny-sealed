d8ca2b2f7deee002b81812fc8ec3849724999a39
Marko Ristin <marko@ristin.ch>
2021-04-26 19:09:06 +0200

    Make mypy accept namespace packages (#10)
    Since `thonnycontrib` is a namespace package, we had problems with mypy
    on Ubuntu and OS X.

--- thonny_seal/__main__.py
+++ thonny_seal/__main__.py
@@ -1,7 +1,7 @@
 """Seal the content of the sealed blocks with a hash."""
 import sys
 
-import thonnycontrib.thonny_sealed.thonny_seal.main
+import thonny_seal.main
 
 if __name__ == "__main__":
-    sys.exit(thonnycontrib.thonny_sealed.thonny_seal.main.entry_point())
+    sys.exit(thonny_seal.main.entry_point())
